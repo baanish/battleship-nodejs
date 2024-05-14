62d41dc98171e72acc1580d52e3bac85f8d6ce72
josh.wakefield <josh.wakefield@epicgames.com>
2024-05-14 16:44:59 -0400

    Added ship initialization to the enemyController

--- EnemyController/enemyController.js
+++ EnemyController/enemyController.js
@@ -20,4 +20,30 @@ module.exports = class EnemyController {
     WasAttempted(attempt) {
         return this.attemptedShots.some((pos) => gameController.PositionsMatch(pos, attempt));
     }
+
+    InitializeEnemyFleet() {
+        this.fleet = gameController.InitializeShips();
+
+        this.fleet[0].addPosition(new position(letters.B, 4));
+        this.fleet[0].addPosition(new position(letters.B, 5));
+        this.fleet[0].addPosition(new position(letters.B, 6));
+        this.fleet[0].addPosition(new position(letters.B, 7));
+        this.fleet[0].addPosition(new position(letters.B, 8));
+
+        this.fleet[1].addPosition(new position(letters.E, 6));
+        this.fleet[1].addPosition(new position(letters.E, 7));
+        this.fleet[1].addPosition(new position(letters.E, 8));
+        this.fleet[1].addPosition(new position(letters.E, 9));
+
+        this.fleet[2].addPosition(new position(letters.A, 3));
+        this.fleet[2].addPosition(new position(letters.B, 3));
+        this.fleet[2].addPosition(new position(letters.C, 3));
+
+        this.fleet[3].addPosition(new position(letters.F, 8));
+        this.fleet[3].addPosition(new position(letters.G, 8));
+        this.fleet[3].addPosition(new position(letters.H, 8));
+
+        this.fleet[4].addPosition(new position(letters.C, 5));
+        this.fleet[4].addPosition(new position(letters.C, 6));
+    }
 }
